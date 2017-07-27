a1f2b7dab7767aac6dbacf790d2a927fa1c035b7
rightusername <dristk@gmail.com>
2017-07-27 14:10:00 +0300

    f

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ const path = require('path');
 const watchLess = require('gulp-watch-less');
  
 const cssMin = require('gulp-css');
+const sprite = require('gulp-sprite-generator');
+const gulpif = require("gulp-if");
+const spritesmith = require('gulp-spritesmith');
+var buffer = require('vinyl-buffer');
  
 gulp.task('css', function(){
   return gulp.src('src/**/*.css')
@@ -28,4 +32,49 @@ gulp.task('less', function () {
 
 gulp.task('watch-less', function() {
     gulp.watch('./less/**/*.less', ['less']);  // Watch all the .less files, then run the less task
+});
+
+
+
+ 
+
+
+// gulp.task('sprites', function() {
+//     var spriteOutput;
+ 
+//     spriteOutput = gulp.src("./css/*.css")
+//         .pipe(sprite({
+//             baseUrl:         "C:/Users/Star-pc/Web_layouts/Adverting_test_task\\img",
+//             spriteSheetName: "sprite.png",
+//             spriteSheetPath: "/dist/image"
+//         }));
+ 
+//     spriteOutput.css.pipe(gulp.dest("./dist/css"));
+//     spriteOutput.img.pipe(gulp.dest("./dist/image"));
+// });
+
+
+// gulp.task('sprites', function() {
+//     var spriteData = 
+//         gulp.src('./img/*.*') // путь, откуда берем картинки для спрайта
+//             .pipe(buffer())
+//             .pipe(spritesmith({
+//                 imgName: 'sprite.png',
+//                 cssName: 'sprite.css',
+//             }));
+
+//     spriteData.img.pipe(gulp.dest('./built/images/')); // путь, куда сохраняем картинку
+//     spriteData.css.pipe(gulp.dest('./built/styles/')); // путь, куда сохраняем стили
+// });
+
+
+gulp.task('sprites', function () {
+    return  gulp.src('./img/*.*')
+                .pipe(spritesmith({
+                    imgName: 'sprite.png',
+                    styleName: 'sprite.less',
+                    imgPath: '../img/sprite.png'
+                }))
+                .pipe(gulpif('*.png', gulp.dest('./dist/img/')))
+                .pipe(gulpif('*.css', gulp.dest('./dist/css/')));
 });
